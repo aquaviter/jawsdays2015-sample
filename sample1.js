127dd8467d37c7464adedf48ac221f843244b375
Hideyo Yoshida <aquaviter@gmail.com>
2015-02-17 23:06:11 +0900

    update sample

--- sample1.js
+++ sample1.js
@@ -2,3 +2,15 @@ var AWS = require('aws-sdk');
 
 var kinesis = new AWS.Kinesis();
 
+var shard_id = "jawsdays2015-handson-track2";
+
+var params = {
+	ShardId: shard_id;
+	ShardIteratorType: 'LATEST'
+};
+
+kinesis.getShardIterator(params, function(err, data){
+	if (err) console.log(err,err.stack);
+	else     console.log(data);
+});
+
