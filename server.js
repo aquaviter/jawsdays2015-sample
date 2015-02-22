a85a462ee35bd8dd49f736258ddbc7857677a99b
Hideyo Yoshida <aquaviter@gmail.com>
2015-02-22 16:02:48 +0900

    v10

--- server.js
+++ server.js
@@ -18,9 +18,28 @@ var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
     var address = socket.handshake.address;
 	console.log('client connected: ' + address.address + ":" + address.port);
-    for(var i = 0; i < 10; i++) {
-        io.sockets.emit("msg", i);
-    }
+    //for(var i = 0; i < 10; i++) {
+    //    io.sockets.emit("msg", i);
+    //}
+    kinesis.describeStream({StreamName:stream},function(err,result){
+        var shards = result.StreamDescription.Shards;
+        console.log(shards);
+        for(var i = 0; i < shards.length; i++){
+            var shardId = shards[i].ShardId;
+            var params = {
+                ShardId: shardId,
+                ShardIteratorType: strategy,
+                StreamName: stream
+                };
+            kinesis.getShardIterator(params,function(err,result){
+                if(err) console.log(err);
+                else {
+                    data = getRecords(kinesis,shardId,result.ShardIterator);
+                    io.socket.emit('msg', data);
+                };
+            });
+        };
+    });
 });
 
 function getKinesisRecords(kinesis,shardId,shardIterator){
