4d65613eeccd7ae9abb1f86d384eff4609201bfb
Hideyo Yoshida <aquaviter@gmail.com>
2015-02-22 11:24:33 +0900

    v3

--- visualization.js
+++ visualization.js
@@ -1,29 +1,31 @@
 var aws = require('aws-sdk');
+var http = require('http');
 
 var region = 'ap-northeast-1';
 var stream = 'jawsdays2015-handson-track2'
 var strategy = 'LATEST';
 var kinesis = new aws.Kinesis({region:region});
 
-kinesis.describeStream({StreamName:stream},function(err,result){
-   var shards = result.StreamDescription.Shards;
-   console.log(shards);
-   for(var i = 0; i < shards.length; i++){
-       var shardId = shards[i].ShardId;
-       var params = {
-         ShardId: shardId,
-         ShardIteratorType: strategy,
-         StreamName: stream
-       };
-       kinesis.getShardIterator(params,function(err,result){
-          if(err) console.log(err);
-          else {
-            r = getRecords(kinesis,shardId,result.ShardIterator);
-            console.log(r.Data);
-          }
-       });
-   }
-});
+http.createServer(function (req,res) {
+  res.writeHead(200, {'Cotent-Type': 'text/plain'});
+
+  kinesis.describeStream({StreamName:stream},function(err,result){
+    var shards = result.StreamDescription.Shards;
+    console.log(shards);
+     for(var i = 0; i < shards.length; i++){
+         var shardId = shards[i].ShardId;
+         var params = {
+            ShardId: shardId,
+            ShardIteratorType: strategy,
+            StreamName: stream
+          };
+          kinesis.getShardIterator(params,function(err,result){
+            if(err) console.log(err);
+            else getRecords(kinesis,shardId,result.ShardIterator);
+        });
+    }
+  });
+}).listen(9000);
 
 function getRecords(kinesis,shardId,shardIterator){
     kinesis.getRecords({ShardIterator: shardIterator, Limit: 10000},function(err,result){
@@ -32,8 +34,7 @@ function getRecords(kinesis,shardId,shardIterator){
             if(result.Records.length){
                 for(var i = 0; i < result.Records.length; i++){
                     r = result.Records[i];
-                    //console.log(shardId + ', ' + r.PartitionKey + ', ' + r.SequenceNumber + ', ' + r.Data);
-                    return r;
+                    console.log(shardId + ', ' + r.PartitionKey + ', ' + r.SequenceNumber + ', ' + r.Data);
                 }
             }
             setTimeout(function() {
@@ -42,5 +43,4 @@ function getRecords(kinesis,shardId,shardIterator){
         }
     })
 }
-
 
