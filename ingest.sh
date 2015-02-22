6e6ffa4bd75ab65cb085c4ef0bb1241098651e07
Hideyo Yoshida <aquaviter@gmail.com>
2015-02-22 23:36:07 +0900

    add ingest.sh

---
+++ ingest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+devicename="testdevice"
+profile="jawsdays2015"
+streamname="jawsdays2015-handson-track2"
+
+while(true); do
+	date=$(date "+%Y-%m-%dT%H:%M:%SZ")
+	value=$(awk -vmin=0 -vmax=100 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+	message="$devicename,$date,$value"
+	message=$(echo $message | openssl enc -e -base64)
+
+	aws kinesis put-record --profile $profile \
+	--stream-name $streamname --data $message --partition-key devicename;
+
+	sleep 5
+
+done
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var aws = require('aws-sdk');
 var fs = require('fs');
-var http = require('http');
 
 var region = 'ap-northeast-1';
 var stream = 'jawsdays2015-handson-track2'
@@ -8,74 +7,51 @@ var stream = 'jawsdays2015-handson-track2'
 var strategy = 'LATEST';
 var kinesis = new aws.Kinesis({region:region});
 
-var server = http.createServer(function (req, res) {
-	res.writeHead(200, {"Content-Type": "text/html"});
-	var html = fs.readFileSync("./client.html", "utf-8");
-	res.end(html);
+var fs = require('fs');
+var app = require('http').createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.end(fs.readFileSync('client.html'));
 }).listen(9000);
 
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection', function (socket) {
-    var address = socket.handshake.address;
-	console.log('client connected: ' + address.address + ":" + address.port);
-    //for(var i = 0; i < 10; i++) {
-    //    io.sockets.emit("msg", i);
-    //}
-    kinesis.describeStream({StreamName:stream},function(err,result){
-        var shards = result.StreamDescription.Shards;
-        for(var i = 0; i < shards.length; i++){
-            var shardId = shards[i].ShardId;
-            var params = {
-                ShardId: shardId,
-                ShardIteratorType: strategy,
-                StreamName: stream
-                };
-            kinesis.getShardIterator(params,function(err,result){
-                if(err) console.log(err);
-                else {
-                    //data = getKinesisRecords(kinesis,shardId,result.ShardIterator);
-                    //io.sockets.emit('msg', data);
-                    console.log("iterator=" + result.ShardIterator);
-                    kinesis.getRecords({ShardIterator: result.ShardIterator, Limit: 100},function(err,result){
-                        if(err) console.log(err);
-                        else {
-                           console.log(result.Records.length); //なぜここが0になるのか？
-                            if(result.Records.length){
-                                for(var i = 0; i < result.Records.length; i++){
-                                 r = result.Records[i];
-                                    console.log(shardId + ', ' + r.PartitionKey + ', ' + r.SequenceNumber + ', ' + r.Data);
-                                    var record = shardId + ', ' + r.PartitionKey + ', ' + r.SequenceNumber + ', ' + r.Data;
-                                    io.sockets.emit('msg', record);
-                                }
-                            }
-                        }
-                    })
-                };
-            });
-        };
-    });
-    socket.on('disconnect', function() {
-        console.log("client Disconnected");
-    });
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket) {
+	socket.on('msg', function(data) {
+		io.sockets.emit('msg'. data);
+	});
 });
 
-function getKinesisRecords(kinesis,shardId,shardIterator){
-    kinesis.getRecords({ShardIterator: shardIterator, Limit: 100},function(err,result){
+kinesis.describeStream({StreamName:stream},function(err,result){
+   var shards = result.StreamDescription.Shards;
+   for(var i = 0; i < shards.length; i++){
+       var shardId = shards[i].ShardId;
+       var params = {
+         ShardId: shardId,
+         ShardIteratorType: strategy,
+         StreamName: stream
+       };
+       kinesis.getShardIterator(params,function(err,result){
+          if(err) console.log(err);
+          else getRecords(kinesis,shardId,result.ShardIterator);
+       });
+   }
+});
+
+function getRecords(kinesis,shardId,shardIterator){
+    kinesis.getRecords({ShardIterator: shardIterator, Limit: 10000},function(err,result){
         if(err) console.log(err);
         else {
-            console.log(result.Records.length);
             if(result.Records.length){
                 for(var i = 0; i < result.Records.length; i++){
                     r = result.Records[i];
+                   	io.sockets.emit('msg', String(r.Data));
                     console.log(shardId + ', ' + r.PartitionKey + ', ' + r.SequenceNumber + ', ' + r.Data);
-                    var records =+ shardId + ', ' + r.PartitionKey + ', ' + r.SequenceNumber + ', ' + r.Data + '\n';
                 }
             }
-            return records;
             setTimeout(function() {
-                getKinesisRecords(kinesis, shardId, result.NextShardIterator);
+                getRecords(kinesis, shardId, result.NextShardIterator);
             },0);
         }
     })
 }
+
+
