e13ac8a963239b645582e71e2c917b74ccefb0ac
Hideyo Yoshida <aquaviter@gmail.com>
2015-02-22 15:36:00 +0900

    v7

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var strategy = 'LATEST';
 var kinesis = new aws.Kinesis({region:region});
 
 var server = http.createServer(function (req, res) {
-	res.writehead(200, {"Content-Type":"text/html"});
+	res.writeHead(200, {"Content-Type":"text/html"});
 	var rs = fs.createReadStream('client.html');
 }).listen(9000);
 
